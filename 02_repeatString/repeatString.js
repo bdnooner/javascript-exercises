495a13d9c644c224f9b1ea78c97782f334fc57e9
Ben Nooner <bdnooner@outlook.com>
2024-12-09 00:17:30 -0700

    completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, numOfTimes) {
+    if(numOfTimes < 0) 
+        return "ERROR";
 
+    
+    return_string = "";
+
+    for(let i = 0; i < numOfTimes; i++){
+        return_string += `${string}`;
+    }
+
+    return return_string;
 };
 
 // Do not edit below this line
