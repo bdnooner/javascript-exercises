78e2c313f96372533fe36f2365e9be4de921d251
Ben Nooner <bdnooner@outlook.com>
2024-12-09 00:29:57 -0700

    Completed exercise 03 [edit] aligned to solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,5 @@
 const reverseString = function(string) {
-    reversed = "";
-
-    for(let i = string.length-1; i >= 0; i--) {
-        reversed += string[i];
-    }
-
-    return reversed;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
