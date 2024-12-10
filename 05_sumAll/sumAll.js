715f9b0afd44f43a64d1c77ac27ef82ea4539120
Ben Nooner <bdnooner@outlook.com>
2024-12-09 22:55:42 -0700

    align with provided solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,19 @@
-const sumAll = function(x, y) {
-    if(x < 0 || y < 0) {
-        return "ERROR";
-    }
-    if(!Number.isInteger(x) || !Number.isInteger(y)) {
-        return "ERROR";
-    }
-    if(typeof x != 'number' || typeof y != 'number'){
-        return "ERROR";
-    }
-    
-    let sum = 0;
-    
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0) return "ERROR";
+  if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
 
-    return sum;
+  if (x > y) {
+    const temp = y;
+    y = x;
+    x = temp;
+  }
+
+  let sum = 0;
+  for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
