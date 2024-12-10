e4004ddf228420d10ad5ac974b03f719ad8796f5
Ben Nooner <bdnooner@outlook.com>
2024-12-09 22:41:46 -0700

    completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    for(let i = 0; i < elements.length; i++){
+        while(arr.indexOf(elements[i]) != -1){
+            arr.splice(arr.indexOf(elements[i]), 1);
+        }
+    }
+    return arr;
+    
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
