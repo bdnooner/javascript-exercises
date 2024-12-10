2ad5bae816c46c2992aac327977a7abb41c7d274
Ben Nooner <bdnooner@outlook.com>
2024-12-09 22:51:37 -0700

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if(x < 0 || y < 0) {
+        return "ERROR";
+    }
+    if(!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    }
+    if(typeof x != 'number' || typeof y != 'number'){
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    
 
+    return sum;
 };
 
 // Do not edit below this line
