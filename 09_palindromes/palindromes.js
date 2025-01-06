168bcfe5c6fcc0d4b5f50875999c88a6c39bedaf
Ben Nooner <bdnooner@outlook.com>
2025-01-05 20:36:06 -0700

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+function removePunctuation(string) {
+    return string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+}
 
+const palindromes = function (string) {
+    let str = removePunctuation(string).replaceAll(" ", "").toLowerCase();
+    let reverse = str.split("").reverse().join("");
+
+    return reverse === str;
 };
 
 // Do not edit below this line
