6049c28b1534f5e7b16690d3a4a515bafcf00a11
Ben Nooner <bdnooner@outlook.com>
2025-01-05 21:01:35 -0700

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if(x < 0) return "OOPS";
+    if (x == 0) return 0;
+    if (x < 2) return 1;
+
+    let count = 1;
+    let current = 1;
+    let prev = 0;
+    let next = 0;
+    while(count < x){
+        console.log(prev);
+        
+        next = prev + current;
+        prev = current;
+        current = next;
+
+        count++;
+    }
+
+    return next;
 
 };
 
