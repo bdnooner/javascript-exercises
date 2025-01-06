b17c70ae69d20b146253d18d71b02611eb0c5ae4
Ben Nooner <bdnooner@outlook.com>
2025-01-05 21:21:43 -0700

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
 
+    return death - birth;
+}
+const findTheOldest = function(people) {
+    people.sort((a, b) =>  
+        getAge(a.yearOfBirth, a.yearOfDeath) - getAge(b.yearOfBirth, b.yearOfDeath))
+        .reverse();
+    return people[0];
 };
 
 // Do not edit below this line
